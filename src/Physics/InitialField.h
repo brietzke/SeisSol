2be8b12ef7bf5fdc19000e105d68e29adba1d3e5
Gilbert Brietzke <brietzke@lrz.de>
2019-04-09 09:29:15 +0200

    remove inline attribute from extern C function

--- src/Physics/InitialField.h
+++ src/Physics/InitialField.h
@@ -2,7 +2,7 @@
 #define INITIALFIELD_H
 
 extern "C" {
-inline void initial_field_planarwave(double time, double x, double y, double z, double* variables);
+void initial_field_planarwave(double time, double x, double y, double z, double* variables);
 }
 
 #endif // INITIALFIELD_H
