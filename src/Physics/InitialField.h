57ac5484bed2b1de0c7d2b03f379ccae34487a92
brietzke <brietzke@users.noreply.github.com>
2019-04-09 10:04:33 +0200

    Update InitialField.h

--- src/Physics/InitialField.h
+++ src/Physics/InitialField.h
@@ -2,7 +2,7 @@
 #define INITIALFIELD_H
 
 extern "C" {
-void initial_field_planarwave(double time, double x, double y, double z, double* variables);
+inline void initial_field_planarwave(double time, double x, double y, double z, double* variables);
 }
 
 #endif // INITIALFIELD_H
